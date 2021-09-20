700578831da034fa7ed9fb4b5f6ac905407c6c45
joselvelez <joselvelez@protonmail.com>
2021-09-20 18:01:51 -0500

    add initial components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import UserContext from './UserContext';
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Root() {
+  const [ user, setUser ] = useState(null);
+  const login = (user) => setUser(user);
+  const logout = () => setUser(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return user ? (
+    <UserContext.Provider value={user}>
+      <MainPage onLogout={logout} />
+    </UserContext.Provider>
+  ) : (
+    <LoginPage onLogin={login} />
+  )
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
